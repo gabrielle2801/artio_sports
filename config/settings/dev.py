2c98ffd365c1943b7c47431b7e7eeff40a1f3c02
gabrielle2801 <gabrielleazadian@gmail.com>
2025-01-03 15:40:40 +0100

    Update config --> coming soon website

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -6,6 +6,16 @@ load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  os.getenv("DB_NAME"),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '3306',
+        }
+}
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY"),
