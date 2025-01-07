658d3c6012b3dce0eb0b8268e4dc77c89fb22b6a
gabrielle2801 <gabrielleazadian@gmail.com>
2025-01-07 13:29:21 +0100

    Update config-dev.py

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -7,7 +7,7 @@ import os
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY"),
+SECRET_KEY = env("SECRET_KEY"),
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
