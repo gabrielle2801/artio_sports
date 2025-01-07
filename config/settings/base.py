413c8b22c4e9b477e821214b12e63fae4acd2605
gabrielle2801 <gabrielleazadian@gmail.com>
2025-01-07 13:21:33 +0100

    Update config email-->website

--- config/settings/base.py
+++ config/settings/base.py
@@ -199,8 +199,8 @@ WAGTAILSEARCH_BACKENDS = {
 # Email configuration 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'artio-sports.com'
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
