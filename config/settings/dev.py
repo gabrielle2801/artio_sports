d3ca05720593342fc0985baed438196ad72a1e31
gabrielle2801 <gabrielleazadian@gmail.com>
2025-01-08 11:22:06 +0100

    Uptade env config

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,14 +1,14 @@
 from .base import *
-import environ
+from config import env
 import os
 
-env = environ.Env() 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY"),
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
