70b5c750fef86e1600046cf74b1c165a26782230
gabrielle2801 <gabrielleazadian@gmail.com>
2025-01-09 15:51:59 +0100

    Update Video

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,8 +4,18 @@ from .base import env
 # from config import env
 
 DEBUG = False
-
 SECRET_KEY = env("SECRET_KEY")
+# Security configuration
+
+# Ensure that the session cookie is only sent by browsers under an HTTPS connection.
+# https://docs.djangoproject.com/en/stable/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE = True
+
+# Ensure that the CSRF cookie is only sent by browsers under an HTTPS connection.
+# https://docs.djangoproject.com/en/stable/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+CSRF_COOKIE_HTTPONLY = True
 
 STATIC_ROOT = env("DJANGO_STATIC_ROOT")
 MEDIA_ROOT = env("DJANGO_MEDIA_ROOT")
