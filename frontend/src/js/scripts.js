efccf39eb3735b5de50674df1fe2c80015365aa3
gabrielle2801 <gabrielleazadian@gmail.com>
2025-01-20 13:31:01 +0100

    Update form

--- frontend/src/js/scripts.js
+++ frontend/src/js/scripts.js
@@ -85,3 +85,24 @@ function mutedAndUnmuted () {
       })
     })()
     
+  
+    $(function(){
+      function rescaleCaptcha(){
+        var width = $('.g-recaptcha').parent().width();
+        var scale;
+        if (width < 302) {
+          scale = width / 302;
+        } else{
+          scale = 1.0; 
+        }
+    
+        $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+        $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+        $('.g-recaptcha').css('transform-origin', '0 0');
+        $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+      }
+    
+      rescaleCaptcha();
+      $( window ).resize(function() { rescaleCaptcha(); });
+    
+    });
