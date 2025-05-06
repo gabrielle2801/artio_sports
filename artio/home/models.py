bc523653aa801acabbe36949978ef9b597d61fdb
gabrielle2801 <gabrielleazadian@gmail.com>
2025-05-06 14:02:35 +0200

    carouel products homepage

--- artio/home/models.py
+++ artio/home/models.py
@@ -1,9 +1,85 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
 
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.admin.panels import (FieldPanel, MultiFieldPanel,
+                                  MultipleChooserPanel)
 from wagtailvideos.edit_handlers import VideoChooserPanel
+
+
+class HomePageGalleryImages(Orderable):
+    """Between 1 and 5 images for the home page carousel."""
+
+    page = ParentalKey("HomePage", related_name="gallery_images")
+
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )  
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+
+class HomePageSetoise(Orderable):
+    page = ParentalKey("HomePage", related_name="gallery_setoise_part1")
+
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+
+class HomePageSetoise2(Orderable):
+    page = ParentalKey("HomePage", related_name="gallery_setoise_part2")
+
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+
+class HomePageSetoise3(Orderable):
+    page = ParentalKey("HomePage", related_name="gallery_setoise_part3")
+
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
 
 
 class HomePage(Page):
@@ -36,49 +112,34 @@ class HomePage(Page):
         help_text="contact Page"
     )
 
-    mention_cta = models.CharField(
-        blank=True, 
-        verbose_name="Mention CTA",
-        max_length=255,
-        help_text="Mention CTA",
-    )
-    mention_cta_link = models.ForeignKey(
-        "wagtailcore.Page",
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="+",
-        verbose_name="mention CTA link",
-        help_text="mention Page"
-    )
-    policy_cta = models.CharField(
-        blank=True, 
-        verbose_name="Mention CTA",
-        max_length=255,
-        help_text="Mention CTA",
-    )
-    policy_cta_link = models.ForeignKey(
-        "wagtailcore.Page",
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="+",
-        verbose_name="policy CTA link",
-        help_text="policy Page"
-    )
-
     content_panels = Page.content_panels + [
         MultiFieldPanel(
             [
                 FieldPanel("contact_cta"),
                 FieldPanel("contact_cta_link"),
-                FieldPanel("mention_cta"),
-                FieldPanel("mention_cta_link"),
-                FieldPanel("policy_cta"),
-                FieldPanel("policy_cta_link"),
             ],
         ),
         FieldPanel('body'),
+        MultipleChooserPanel(
+            'gallery_images', 
+            label="Gallery images", 
+            chooser_field_name="image"
+        ),
+        MultipleChooserPanel(
+            'gallery_setoise_part1', 
+            label="Gallery images setoise", 
+            chooser_field_name="image"
+        ),
+        MultipleChooserPanel(
+            'gallery_setoise_part2', 
+            label="Gallery images setoise 2", 
+            chooser_field_name="image"
+        ),
+        MultipleChooserPanel(
+            'gallery_setoise_part3', 
+            label="Gallery images setoise 3", 
+            chooser_field_name="image"
+        )
     ]
 
 
